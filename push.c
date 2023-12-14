cad2db71da19fe02df2232dda38f66abab4a4999
MARWANANO32 <marwanano32@gmail.com>
2023-12-15 01:37:45 +0200

    MARWAN

---
+++ push.c
@@ -0,0 +1,40 @@
+#include "monty."
+/**
+ * push_s - the function that add element for stack
+ * @top: the top of stack
+ * @count: the line number
+ */
+void push_s(stack_t **top, unsigned int count)
+{
+	int n;
+	int j = 0, flags = 0;
+
+	if (cv.arg)
+	{
+		if (cv.arg[0] == '-')
+			j++;
+		for (; cv.arg[j] != '\0'; j++)
+		{
+			if (cv.arg[j] > 57 || cv.arg[j] < 48)
+				flags = 1;
+			if (flags == 1)
+				fprintf(stderr, "L%d: usage: push integer\n", count);
+				fclose(cv.file);
+				free(cv.content);
+				free_stack(*head);
+				exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", count);
+		fclose(cv.file);
+		free(cv.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(cv.arg);
+	if (cv.flag == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
+}
