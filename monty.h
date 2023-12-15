d19aa1e618a2c606547f42de40538c79d5f3cdc2
MARWANANO32 <marwanano32@gmail.com>
2023-12-15 02:35:26 +0200

    MARWAN

--- monty.h
+++ monty.h
@@ -1,6 +1,7 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -8,6 +9,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdarg.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
